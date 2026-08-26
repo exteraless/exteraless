876f4000ddb67a14db2290798c9f5e7d9b324a30
xmdnx <smcartix@gmail.com>
2026-08-26 16:40:25 +0300

    fix: blinking avatar when online indicator enabled

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
@@ -2120,7 +2120,7 @@ public class ChatMessageCell extends BaseCell implements SeekBar.SeekBarDelegate
         onlineStatusPaint.setColor(onlineColor);
         onlineStatusPath.rewind();
         onlineStatusPath.addCircle(x - offset, y - offset, radius, Path.Direction.CW);
-        onlineStatusPath.toggleInverseFillType();
+        onlineStatusPath.setFillType(Path.FillType.INVERSE_WINDING);
 
         canvas.save();
         canvas.clipPath(onlineStatusPath);
